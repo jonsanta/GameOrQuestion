10db43974086136d5abb0953ae7e3fe79002e4cc
jonsanta <jon.santamaria1995@gmail.com>
2022-04-20 23:10:58 +0200

    Refactorized

--- Assets/Scripts/Questions/Question.cs
+++ Assets/Scripts/Questions/Question.cs
@@ -1,83 +1,72 @@
 using System.Xml;
 using UnityEngine.UI;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Question : MonoBehaviour
 {
     public GameObject questionGObj;
     public GameObject[] buttons = new GameObject[4];
 
-    public  string[] QuestionArray; //saves selected question
-    private int[] order = new int[4]; //used for mixing the answers without losing the correct one
-    private int orderIndex = 0; 
-    private bool orderFlag = false; //this flag will be true if Random.Range repeats a number
+    private string question = null;
+
+    public List<int> order = new List<int>();
 
     private bool answerSelected = false;
-
-    void Start()
-    {
-        QuestionArray = readXML(); //The question and answers will be filled
-    }
 
 
     void Update()
     {
-        questionGObj.GetComponent<Text>().text = QuestionArray[0]; //Question displayed
-
-        while (orderFlag || orderIndex < 4) //if flag is true or still need more numbers
+        while (order.Count < 4)
         {
-            order[orderIndex] = Random.Range(0, 4); //new random number
-            orderFlag = false;
-
-            for (int x = 0; x < orderIndex; x++) //loop the numbers we saved
-            {
-                if (order[orderIndex] == order[x] && orderIndex != x) orderFlag = true; //if repeats number flag true
-            }
-
-            if (orderFlag == false) orderIndex += 1; //if not, add the number
+            int temp = Random.Range(0, 4);//new random number
+            if (!order.Contains(temp)) order.Add(temp);
         }
 
+        while(question == null)readXML(order.ToArray()); //The question and answers will be filled
+        questionGObj.GetComponent<Text>().text = question; //Question displayed
 
-        for (int x = 0; x < 4; x++)
-        {
-            if (order[x] == 0) buttons[x].GetComponent<Answer>().setIsTrueAnswer(); //sets the true answer
-            buttons[x].GetComponentInChildren<Text>().text = QuestionArray[order[x] + 1]; //fills buttons whit the answer text
-            buttons[x].GetComponent<Answer>().setQuestion(this);
-        }
-
-        if (answerSelected && buttons[3].GetComponent<Button>().enabled == true) // if user selects an answer ---> all buttons disabled
-        {
-            for (int x = 0; x < buttons.Length; x++)
-            {
-                buttons[x].GetComponent<Button>().enabled = false;
-            }
-        }
+        // if user selects an answer ---> all buttons disabled
+        if (answerSelected) 
+            foreach(GameObject btn in buttons) 
+                btn.GetComponent<Button>().enabled = false;
     }
 
-    string[] readXML()
+    void readXML(int[] order)
     {
         string[] arr = new string[5]; // Empty array
 
         XmlDocument xdoc = new XmlDocument();
         xdoc.LoadXml(Resources.Load<TextAsset>("Questions").text); //load XML document from TextAssets
 
-        XmlNode xmlRootNode = xdoc.SelectSingleNode("Game"); //Root Node
-        XmlNodeList nOfQuestions = xmlRootNode.ChildNodes; //Number of questions
+        XmlNodeList questions = xdoc.SelectSingleNode("Game").ChildNodes; //Root element childs
 
-        int index = (int)Random.Range(1f, nOfQuestions.Count); //Select random question
+        int index = (int)Random.Range(1f, questions.Count); //Select random question
 
-        foreach (XmlElement item in nOfQuestions)
+        foreach (XmlElement item in questions)
         {
             if (item.Attributes["id"].Value == index.ToString())
             {
                 for(int x = 0; x < item.ChildNodes.Count; x++)
                 {
-                    arr[x] = item.ChildNodes[x].InnerText; //arr[0] = questions, arr[1] = correct answer, arr[2,3,4] = incorrect answer
+                    XmlNode node = item.ChildNodes[x]; //save each child in variable
+                    switch(node.Name)
+                    {
+                        //if xml element name
+                        case "Question": 
+                            question = node.InnerText; //sets question
+                            break;
+                        case "CorrectAnswer":
+                            buttons[order[x-1]].GetComponent<Answer>().setIsTrueAnswer(); //sets correct answer
+                            goto default;
+                        default:
+                            buttons[order[x-1]].GetComponentInChildren<Text>().text = node.InnerText; //fills buttons whit the answer text
+                            buttons[order[x-1]].GetComponent<Answer>().setQuestion(this);
+                            break;
+                    }
                 }
             }
         }
-
-        return arr;
     }
 
     public void setAnswerSelected(bool answerSelected)
